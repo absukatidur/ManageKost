fd405de5901ff099dd255f81044d6f29fa2a3517
absukatidur <andika.azk96@gmail.com>
2026-05-21 12:36:43 +0800

    Spliting HTML into reusable PHP component

---
+++ components/admin_topbar.php
@@ -0,0 +1,18 @@
+<div id="main">
+  <header id="topbar">
+    <div class="topbar-left">
+      <button class="btn-icon" id="menu-toggle" onclick="toggleSidebar()">
+        <i data-lucide="menu" style="width:16px;height:16px"></i>
+      </button>
+      <span class="topbar-title" id="page-title">Dashboard</span>
+    </div>
+    <div class="topbar-right">
+      <div class="avatar">AD</div>
+    </div>
+  </header>
+
+  <main id="page-content"></main>
+</div>
+
+<div id="modal-container"></div>
+<div id="toast-container"></div>
